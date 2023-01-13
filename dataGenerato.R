a44b75e5d44017a899d0cdf8044e0b822818a646
Sam Boots <samsaracb23@gmail.com>
2023-01-13 17:29:57 -0500

    Added "row.names=F" in write.csv command.

--- dataGenerato.R
+++ dataGenerato.R
@@ -1,1 +1,22 @@
-SpeciesName<-c()
+#1
+#Random sample of 100 species names from vector of 5 species names
+Spec_names<-sample(c("Felis bingus","Felis lilacus","Alois gorus","Babius busteria","Polka dotdeadius"),size=100,replace=T)
+
+#2
+#Random sample of 100 values for limb width
+Limb_width<-sample(rnorm(1000,mean=8,sd=2),size=100,replace=F)
+
+#3
+#Random sample of 100 values for limb length
+Limb_length<-sample(rnorm(1000,mean=30,sd=6),size=100,replace=F)
+
+#4
+#Random sample of 100 values for observer
+Observer_names<-sample(c("Sam B","Mike R","Lee N"),size=100,replace=T)
+
+#5
+#Combining vectors into data frame object
+LimbData<-data.frame(Organism=Spec_names,Limb_width=Limb_width,Limb_length=Limb_length,Observer=Observer_names)
+
+#Exporting data frame to csv
+write.csv(LimbData,"measurements.csv", row.names=FALSE)
--- volumeEstimato.R
+++ volumeEstimato.R
@@ -1,7 +1,9 @@
 library(dplyr)
 
-#import data from csv and add new column calculating volume from limb-width and limb-volume, estimating volume with assumption each limb is a cylinder
+#import data from csv
 read.csv("measurements.csv") %>%
+  #add new column calculating volume from limb-width and limb-volume, estimating volume with assumption each limb is a cylinder
   mutate(Volume=(pi*Limb_length*(Limb_width/2)^2)) %>%
-  write.csv("measurements.csv")
+  #export new csv including calculated limb volume
+  write.csv("measurements.csv", row.names=F)
 
